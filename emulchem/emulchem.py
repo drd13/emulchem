f2518ae65826bdc3eb602dc184db58f60f38088b
Damien de Mijolla <damiendemijolla@gmail.com>
2019-09-21 15:56:54 +0100

    made paths relative and made outofbounds errors for radex give actual bounds

--- emulchem/emulchem.py
+++ emulchem/emulchem.py
@@ -14,8 +14,9 @@ print(path_emulchem)
 
 def molecule_list():
     """Returns an array containing a list of all of the molecules included in the emulator"""
-    files = glob.glob("data/chem/scaler*.py")
-    molecules = [f.split(".")[0][6:] for x in files]
+    query = os.path.join(path_emulchem,"data","chem","scaler*.p")
+    files = glob.glob(query)
+    molecules = [f.split(".")[0][6:] for f in files]
     return molecules
 
 class Emulator():
@@ -36,10 +37,12 @@ class ChemistryEmulator(Emulator):
     def __init__(self,specie):
         Emulator.__init__(self,specie)
         self.neural_network = NeuralNet(input_size=6,hidden_size=200,hidden_size2=100,hidden_size3=50,num_outputs=1)
-        self.neural_network.load_state_dict(torch.load(os.path.join(path_emulchem,"data/chem/network{}".format(self.specie))))
-        with open(os.path.join(path_emulchem,"data/chem/minMaxScaler.p"),"rb") as f:
+        path_chem = os.path.join(path_emulchem,"data","chem")
+        #self.neural_network.load_state_dict(torch.load(os.path.join(path_emulchem,"data/chem/network{}".format(self.specie))))
+        self.neural_network.load_state_dict(torch.load(os.path.join(path_chem,"network{}".format(self.specie))))
+        with open(os.path.join(path_chem,"minMaxScaler.p"),"rb") as f:
             self.input_scaler = pickle.load(f)
-        with open(os.path.join(path_emulchem,"data/chem/scaler{}.p".format(self.specie)),"rb") as f:
+        with open(os.path.join(path_chem,"scaler{}.p".format(self.specie)),"rb") as f:
             self.output_scaler= pickle.load(f)
     
     def get_prediction(self,radfield,zeta,density,av,temperature,metallicity):
@@ -64,26 +67,20 @@ class ChemistryEmulator(Emulator):
         zeta_bounds = [1,10**3]
 
         out_of_bounds = []
+        bounds_msg = "{} was {} which is outside of the emulator bounds of {} to {}\n"
 
         if av<av_bounds[0] or av>av_bounds[1]:
-            #out_of_bounds.append("av")
-            out_of_bounds.append("av was {} which is outside of the emulator bounds of {} to {}\n".format(av,av_bounds[0],av_bounds[1]))
+            out_of_bounds.append(bounds_msg.format("av",av,av_bounds[0],av_bounds[1]))
         if density<density_bounds[0] or density>10**6:
-            #out_of_bounds.append("density")
-            out_of_bounds.append("density was {} which is outside of the emulator bounds of {} to {}\n".format(density,density_bounds[0],density_bounds[1]))
+            out_of_bounds.append(bounds_msg.format("density",density,density_bounds[0],density_bounds[1]))
         if temperature<temperature_bounds[0] or temperature>temperature_bounds[1]:
-            #out_of_bounds.append("temperature")
-            out_of_bounds.append("temperature was {} which is outside of the emulator bounds of {} to {}\n".format(temperature,temperature_bounds[0],temperature_bounds[1]))
+            out_of_bounds.append(bounds_msg.format("temperature",temperature,temperature_bounds[0],temperature_bounds[1]))
         if metallicity<metallicity_bounds[0] or metallicity>metallicity_bounds[1]:
-            #out_of_bounds.append("metallicity")
-            out_of_bounds.append("metallicity was {} which is outside of the emulator bounds of {} to {}\n".format(metallicity,metallicity_bounds[0],metallicity_bounds[1]))
+            out_of_bounds.append(bounds_msg.format("metallicity",metallicity,metallicity_bounds[0],metallicity_bounds[1]))
         if radfield<radfield_bounds[0] or radfield>radfield_bounds[1]:
-            #out_of_bounds.append("radfield")
-            out_of_bounds.append("radfield was {} which is outside of the emulator bounds of {} to {}\n".format(radfield,radfield_bounds[0],radfield_bounds[1]))
-
+            out_of_bounds.append(bounds_msg.format("radfield",radfield,radfield_bounds[0],radfield_bounds[1]))
         if zeta<zeta_bounds[0] or zeta>zeta_bounds[1]:
-            #out_of_bounds.append("zeta")
-            out_of_bounds.append("zeta was {} which is outside of the emulator bounds of {} to {}\n".format(zeta,zeta_bounds[0],zeta_bounds[1]))
+            out_of_bounds.append(bounds_msg.format("zeta",zeta,zeta_bounds[0],zeta_bounds[1]))
 
         if len(out_of_bounds) !=0:
             raise Exception("".join(out_of_bounds))
@@ -98,12 +95,12 @@ class RadexEmulator(Emulator):
         Emulator.__init__(self,specie)
         self.transition  = transition
         self.neural_network = NeuralNet(input_size=3,hidden_size=200,hidden_size2=100,hidden_size3=50,num_outputs=1)
-        self.neural_network.load_state_dict(torch.load("/home/drd13/Project/analysis/modelsRadex/network{}{}".format(self.specie,self.transition)))
-        self.input_scaler = pickle.load(open(os.path.join(path_emulchem,"data/rad/minMaxScaler{}{}.p".format(self.specie,self.transition)), "rb"))
-        self.output_scaler = pickle.load(open(os.path.join(path_emulchem,"data/rad/scaler{}{}.p".format(self.specie,self.transition)), "rb"))
-        
-        #self.input_scaler = pickle.load(open("/home/drd13/Project/analysis/modelsRadex/minMaxScaler{}{}.p".format(self.specie,self.transition), "rb"))
-        #self.output_scaler = pickle.load(open("/home/drd13/Project/analysis/modelsRadex/scaler{}{}.p".format(self.specie,self.transition),"rb" ))
+        path_rad = os.path.join(path_emulchem,"data","rad")
+        self.neural_network.load_state_dict(torch.load(os.path.join(path_rad,"network{}{}".format(self.specie,self.transition))))
+        with open(os.path.join(path_rad,"minMaxScaler{}{}.p".format(self.specie,self.transition)),"rb") as f:
+            self.input_scaler = pickle.load(f)
+        with open(os.path.join(path_rad,"scaler{}{}.p".format(self.specie,self.transition)),"rb") as f:
+            self.output_scaler= pickle.load(f)
 
     def get_prediction(self,temperature,density,column_density,line_width=1):
         """Temperature:Kelvin
@@ -113,10 +110,46 @@ class RadexEmulator(Emulator):
         """
         self.check_bounds(temperature,density,column_density,line_width)
         x = [temperature,density,np.log10(column_density/line_width)]
-        #y = np.power(10,self.get_prediction_list(x))*line_width
         y = self.get_prediction_list(x)*line_width
         return y
 
+    def check_bounds(self,temperature,density,column_density,line_width):
+        density_bounds = [10**4,10**6]
+        temperature_bounds = [10,200]
+        CO_bounds = [10**13,10**19]
+        CS_bounds = [10**10,10**18]
+        HCOplus_bounds = [10**8,10**15]
+        HCN_bounds = [10**9,10**17]
+
+        out_of_bounds = []
+        scaled_column_density = column_density/line_width
+
+        bounds_msg = "{} was {} which is outside of the emulator bounds of {} to {}\n"
+
+        if density<density_bounds[0] or density>10**6:
+            out_of_bounds.append(bounds_msg.format("density",density,density_bounds[0],density_bounds[1]))
+        if temperature<temperature_bounds[0] or temperature>temperature_bounds[1]: 
+            out_of_bounds.append(bounds_msg.format("temperature",temperature,temperature_bounds[0],temperature_bounds[1]))
+        if self.specie=="CO":
+            if scaled_column_density<CO_bounds[0] or scaled_column_density>CO_bounds[1]:
+                out_of_bounds.append(bounds_msg.format("scaled column density",scaled_column_density,CO_bounds[0],CO_bounds[1]))
+        elif self.specie=="CS":
+            if scaled_column_density<CS_bounds[0] or scaled_column_density>CS_bounds[1]:
+                out_of_bounds.append(bounds_msg.format("scaled column density",scaled_column_density,CS_bounds[0],CS_bounds[1]))
+        elif self.specie=="HCO+":
+            if scaled_column_density<HCOplus_bounds[0] or scaled_column_density>HCOplus_bounds[1]:
+                out_of_bounds.append(bounds_msg.format("scaled column density",scaled_column_density,HCOplus_bounds[0],HCOplus_bounds[1]))
+        elif self.specie=="HCN":
+            if scaled_column_density<HCN_bounds[0] or scaled_column_density>HCN_bounds[1]:
+                out_of_bounds.append(bounds_msg.format("scaled column density",scaled_column_density,HCN_bounds[0],HCN_bounds[1]))
+
+        if len(out_of_bounds) !=0:
+            raise Exception("".join(out_of_bounds))
+
+
+
+
+"""
     def check_bounds(self,temperature,density,column_density,line_width):
         out_of_bounds = []
         scaled_column_density = column_density/line_width
@@ -142,5 +175,5 @@ class RadexEmulator(Emulator):
         if len(out_of_bounds) !=0:
             raise Exception(", ".join(out_of_bounds)+ " out of emulator usable bounds")
 
-
+"""
 
