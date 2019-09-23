76c069ff005562cde920ca4cf76cc1cb99cc6beb
Damien de Mijolla <damiendemijolla@gmail.com>
2019-09-23 11:52:54 +0100

    building docs,adding mock modules to pass build

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
 
-MOCK_MODULES = ['numpy', 'torch',"torchvision","torch.nn"]
+MOCK_MODULES = ['numpy', 'torch',"torchvision","torch.nn","torchvision.transforms"]
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
